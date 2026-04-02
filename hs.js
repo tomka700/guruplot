e801bb3f1d3bb64d783e26c18e4aa55386047d18
Tamás Fülöp <125925723+tomka700@users.noreply.github.com>
2026-04-02 20:15:02 +0200

    Update hs.js

--- hs.js
+++ hs.js
@@ -1,6 +1,6 @@
 (() => 
   [...document.querySelectorAll('table.table tbody tr')].map(row => ({
-    archetype: row.querySelector('td.decklist-info a.deck-title').textContent.trim(),
+    name: row.querySelector('td.decklist-info a.deck-title').textContent.trim(),
     winrate: parseFloat(row.cells[1].querySelector('span.basic-black-text').textContent),
     pop: parseInt(
       row.cells[2].textContent.match(/\(([\d,]+)\)/)[1].replace(/,/g, '')
