5c82fe35f45e738181759d382b17117988457777
Tamás Fülöp <125925723+tomka700@users.noreply.github.com>
2026-04-10 02:32:06 +0200

    auto detect rank bracket

--- hsplot.py
+++ hsplot.py
@@ -5,7 +5,9 @@ data = [
     # replace this with the input
 ]
 if not data: raise ValueError("No Data")
-df = pd.DataFrame(data)
+rank = data[0].replace("_", " ")
+rank = rank if "top" in rank else rank.replace("diamond ", "d").replace("to", "-").replace(" legend", "l").replace("1", "d1")
+df = pd.DataFrame(data[1])
 
 class_colors = {
     "Death Knight": "#6C699A",
@@ -36,7 +38,7 @@ df["is_hl"] = df["name"].str.contains("HL|Highlander")
 
 plt.figure(figsize=(10, 6))
 plt.scatter(df["pop"], df["winrate"], c=df["color"], s=90, edgecolor="black", alpha=0.9, zorder=3)
-plt.xlabel("# of games (top 100 this patch)")
+plt.xlabel(f"# of games ({rank} this patch)")
 plt.xscale("log")
 plt.ylabel("Win Rate (%)")
 l = [m*z for e in range(len(str(min(df["pop"])))-1,len(str(max(df["pop"])))) for m in range(1,10) if min(df["pop"])-(z:=10**e) < m*z < max(df["pop"])+z]
