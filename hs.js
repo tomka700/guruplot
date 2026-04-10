fdd836af145378112542df596c6d9dbdd8cf6764
Tamás Fülöp <125925723+tomka700@users.noreply.github.com>
2026-04-10 02:31:01 +0200

    auto detect rank bracket

--- hs.js
+++ hs.js
@@ -1,10 +1,11 @@
-(() => 
+(() => [
+  new URLSearchParams(location.search).get("rank") || "diamond_to_legend",
   [...document.querySelectorAll("table.table tbody tr")].map(row => ({
     name: row.cells[0].textContent.trim(),
     winrate: parseFloat(row.cells[1].textContent),
-    pop: parseInt(row.cells[2].textContent.match(/\(([\d,]+)\)/)[1]),
+    pop: parseInt(row.cells[2].textContent.match(/\(([\d,]+)\)/)[1].replace(/,/g, '')),
     turns: parseFloat(row.cells[3].textContent),
     duration: parseFloat(row.cells[4].textContent),
     climb_speed: parseFloat(row.cells[5].textContent)
   }))
-)();
+])();
