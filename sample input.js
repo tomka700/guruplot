ef8fb1aeed0ab1941bc48c48a83a6f37d226e33e
Tamás Fülöp <125925723+tomka700@users.noreply.github.com>
2026-05-12 20:28:54 +0200

    Create sample input.js

---
+++ sample input.js
@@ -0,0 +1,373 @@
+[
+    "diamond_to_legend",
+    [
+        {
+            "name": "Discolock",
+            "winrate": 64,
+            "pop": 27071,
+            "turns": 5.6,
+            "duration": 4.4,
+            "climb_speed": 3.79
+        },
+        {
+            "name": "CtA Paladin",
+            "winrate": 63.3,
+            "pop": 4446,
+            "turns": 6.5,
+            "duration": 5,
+            "climb_speed": 3.19
+        },
+        {
+            "name": "Big Shaman",
+            "winrate": 62.9,
+            "pop": 1740,
+            "turns": 6.5,
+            "duration": 5,
+            "climb_speed": 3.1
+        },
+        {
+            "name": "Ohn'ahra Big Shaman",
+            "winrate": 62.8,
+            "pop": 1758,
+            "turns": 6.5,
+            "duration": 5.2,
+            "climb_speed": 2.95
+        },
+        {
+            "name": "Seedlock",
+            "winrate": 62.3,
+            "pop": 2101,
+            "turns": 6.5,
+            "duration": 6.5,
+            "climb_speed": 2.27
+        },
+        {
+            "name": "Odd Questline Hunter",
+            "winrate": 61.9,
+            "pop": 3407,
+            "turns": 6.7,
+            "duration": 6.2,
+            "climb_speed": 2.31
+        },
+        {
+            "name": "XL Demon Boarlock",
+            "winrate": 61.8,
+            "pop": 3966,
+            "turns": 7.5,
+            "duration": 5.8,
+            "climb_speed": 2.43
+        },
+        {
+            "name": "Lynessa Libram Paladin",
+            "winrate": 61.5,
+            "pop": 8368,
+            "turns": 6.6,
+            "duration": 5.6,
+            "climb_speed": 2.45
+        },
+        {
+            "name": "Boar Hunter",
+            "winrate": 61,
+            "pop": 4122,
+            "turns": 7.2,
+            "duration": 6.1,
+            "climb_speed": 2.16
+        },
+        {
+            "name": "XL Shadow Priest",
+            "winrate": 60.8,
+            "pop": 3094,
+            "turns": 6.4,
+            "duration": 4.7,
+            "climb_speed": 2.73
+        },
+        {
+            "name": "Igneous Odyn Warrior",
+            "winrate": 59.8,
+            "pop": 4468,
+            "turns": 8.2,
+            "duration": 6.7,
+            "climb_speed": 1.75
+        },
+        {
+            "name": "XL Seedlock",
+            "winrate": 59.8,
+            "pop": 3564,
+            "turns": 7.4,
+            "duration": 6.9,
+            "climb_speed": 1.69
+        },
+        {
+            "name": "LC Quest Priest",
+            "winrate": 59.6,
+            "pop": 5918,
+            "turns": 7.2,
+            "duration": 6.6,
+            "climb_speed": 1.74
+        },
+        {
+            "name": "Insanity Warlock",
+            "winrate": 59.3,
+            "pop": 1701,
+            "turns": 6.8,
+            "duration": 5.9,
+            "climb_speed": 1.88
+        },
+        {
+            "name": "Boarlock",
+            "winrate": 58.6,
+            "pop": 15698,
+            "turns": 6.2,
+            "duration": 4.6,
+            "climb_speed": 2.23
+        },
+        {
+            "name": "XL HL Exodia Mage",
+            "winrate": 58.4,
+            "pop": 10935,
+            "turns": 9.4,
+            "duration": 8.8,
+            "climb_speed": 1.15
+        },
+        {
+            "name": "XL HL Dragon Druid",
+            "winrate": 58,
+            "pop": 13269,
+            "turns": 8.7,
+            "duration": 7.9,
+            "climb_speed": 1.21
+        },
+        {
+            "name": "777 Miracle Rogue",
+            "winrate": 57.9,
+            "pop": 15823,
+            "turns": 5.9,
+            "duration": 5.7,
+            "climb_speed": 1.68
+        },
+        {
+            "name": "XL HL Tick Tock Warlock",
+            "winrate": 57.9,
+            "pop": 18280,
+            "turns": 9,
+            "duration": 7.5,
+            "climb_speed": 1.25
+        },
+        {
+            "name": "XL HL Shudder Shaman",
+            "winrate": 57.7,
+            "pop": 4603,
+            "turns": 9.3,
+            "duration": 7.8,
+            "climb_speed": 1.18
+        },
+        {
+            "name": "XL HL Igneous Warrior",
+            "winrate": 57.3,
+            "pop": 7252,
+            "turns": 10.1,
+            "duration": 9.1,
+            "climb_speed": 0.97
+        },
+        {
+            "name": "Token Broxigar DH",
+            "winrate": 57.3,
+            "pop": 7060,
+            "turns": 6.4,
+            "duration": 6.5,
+            "climb_speed": 1.36
+        },
+        {
+            "name": "XL Deios Druid",
+            "winrate": 57.3,
+            "pop": 1328,
+            "turns": 6.2,
+            "duration": 5.1,
+            "climb_speed": 1.73
+        },
+        {
+            "name": "XL Highlander Druid",
+            "winrate": 56.7,
+            "pop": 2117,
+            "turns": 9.1,
+            "duration": 8.5,
+            "climb_speed": 0.95
+        },
+        {
+            "name": "XL JtU Quest Mage",
+            "winrate": 56.6,
+            "pop": 5010,
+            "turns": 10.7,
+            "duration": 9.7,
+            "climb_speed": 0.82
+        },
+        {
+            "name": "XL Dragon Druid",
+            "winrate": 56.3,
+            "pop": 2962,
+            "turns": 7.5,
+            "duration": 6.3,
+            "climb_speed": 1.19
+        },
+        {
+            "name": "Barnes Druid",
+            "winrate": 56.2,
+            "pop": 1046,
+            "turns": 4.9,
+            "duration": 3.8,
+            "climb_speed": 1.97
+        },
+        {
+            "name": "XL HL Thief Priest",
+            "winrate": 56.1,
+            "pop": 2369,
+            "turns": 9.8,
+            "duration": 8.9,
+            "climb_speed": 0.82
+        },
+        {
+            "name": "XL HL Tog Druid",
+            "winrate": 55.9,
+            "pop": 6165,
+            "turns": 9.5,
+            "duration": 9.3,
+            "climb_speed": 0.77
+        },
+        {
+            "name": "XL LC Quest Priest",
+            "winrate": 55.9,
+            "pop": 1038,
+            "turns": 8.6,
+            "duration": 7.9,
+            "climb_speed": 0.9
+        },
+        {
+            "name": "XL HL Shadow Priest",
+            "winrate": 55.9,
+            "pop": 2025,
+            "turns": 9.4,
+            "duration": 8.1,
+            "climb_speed": 0.87
+        },
+        {
+            "name": "Velarok Rogue",
+            "winrate": 55.7,
+            "pop": 1802,
+            "turns": 6.1,
+            "duration": 5.8,
+            "climb_speed": 1.17
+        },
+        {
+            "name": "XL HL Questline Priest",
+            "winrate": 54.4,
+            "pop": 2372,
+            "turns": 10.1,
+            "duration": 9.7,
+            "climb_speed": 0.54
+        },
+        {
+            "name": "XL HL LC Quest Warrior",
+            "winrate": 54.3,
+            "pop": 5621,
+            "turns": 9.5,
+            "duration": 8.7,
+            "climb_speed": 0.6
+        },
+        {
+            "name": "XL Mill Druid",
+            "winrate": 53.9,
+            "pop": 6414,
+            "turns": 9.2,
+            "duration": 8.9,
+            "climb_speed": 0.53
+        },
+        {
+            "name": "Quasar Rogue",
+            "winrate": 53.8,
+            "pop": 14699,
+            "turns": 5,
+            "duration": 3.9,
+            "climb_speed": 1.17
+        },
+        {
+            "name": "XL Highlander Warlock",
+            "winrate": 53.8,
+            "pop": 2769,
+            "turns": 9.6,
+            "duration": 7.9,
+            "climb_speed": 0.59
+        },
+        {
+            "name": "Shadow Priest",
+            "winrate": 53.5,
+            "pop": 2779,
+            "turns": 6.1,
+            "duration": 5.1,
+            "climb_speed": 0.83
+        },
+        {
+            "name": "Miracle Rogue",
+            "winrate": 53.4,
+            "pop": 1000,
+            "turns": 5.5,
+            "duration": 5.2,
+            "climb_speed": 0.79
+        },
+        {
+            "name": "XL Hostage Mage",
+            "winrate": 53.3,
+            "pop": 2236,
+            "turns": 11,
+            "duration": 11,
+            "climb_speed": 0.36
+        },
+        {
+            "name": "XL HL Thief Rogue",
+            "winrate": 52.8,
+            "pop": 1379,
+            "turns": 9.6,
+            "duration": 10.3,
+            "climb_speed": 0.33
+        },
+        {
+            "name": "Champions Druid",
+            "winrate": 52.3,
+            "pop": 1931,
+            "turns": 5.9,
+            "duration": 4.9,
+            "climb_speed": 0.56
+        },
+        {
+            "name": "Even Shaman",
+            "winrate": 50.9,
+            "pop": 1510,
+            "turns": 7.4,
+            "duration": 6.4,
+            "climb_speed": 0.17
+        },
+        {
+            "name": "Elemental Mage",
+            "winrate": 49.4,
+            "pop": 1141,
+            "turns": 8.1,
+            "duration": 7.4,
+            "climb_speed": -0.09
+        },
+        {
+            "name": "Alex Rogue",
+            "winrate": 46.8,
+            "pop": 1005,
+            "turns": 5.8,
+            "duration": 10.5,
+            "climb_speed": -0.37
+        },
+        {
+            "name": "JtU Quest Mage",
+            "winrate": 42.8,
+            "pop": 3458,
+            "turns": 6,
+            "duration": 5,
+            "climb_speed": -1.72
+        }
+    ]
+]
